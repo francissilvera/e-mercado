d5f03d1dbc98dce5a1a36947c1b3d60f5ad489dc
francissilvera <88556900+francissilvera@users.noreply.github.com>
2021-08-20 14:34:18 -0300

    entrega1

--- js/login.js
+++ js/login.js
@@ -1,6 +1,34 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
-});
+document.addEventListener("DOMContentLoaded", function(e) {
+
+    document.getElementById("submit").addEventListener("click", function() {
+
+        let inputEmail = document.getElementById("inputEmail");
+        let inputPassword = document.getElementById("inputPassword");
+        let camposCompletos = true;
+
+        if (inputEmail.value === "") {
+            camposCompletos = false;
+            inputEmail.classList.add("invalid");
+        }
+        else {
+            inputEmail.classList.remove("invalid");
+        } 
+        if (inputPassword.value === "") {
+            camposCompletos = false;
+            inputPassword.classList.add("invalid");
+        }
+        else {
+            inputPassword.classList.remove("invalid");
+        } 
+
+        if (camposCompletos) {
+            window.location = "inicio.html"
+        }
+        else {
+            alert("¡Debes ingresar todos tus datos!")
+        }
+})});  
--- js/products.js
+++ js/products.js
@@ -1,6 +1,42 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
+var productosArray = [];
+
+function mostrarProductos(array) {
+
+    let contenido = "<br>";
+
+    for (let i = 0; i < array.length; i++) {
+        let producto = array[i];
+
+        contenido += ` 
+        <div class="row">
+                    <div class="col-3"> 
+                        <img src="` + producto.imgSrc + `" alt="` + producto.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ producto.name +`</h4>
+                        </div>
+                        <p class="mb-1">` + producto.description + `</p>
+                        <p class="mb-1">` + producto.cost + ` USD </p>
+                    </div>
+                </div>`
+    }
+
+    document.getElementById("productos").innerHTML = contenido;
+}
+
 document.addEventListener("DOMContentLoaded", function (e) {
 
+    getJSONData(PRODUCTS_URL).then(function (resultado) {
+        if (resultado.status === "ok") {
+            productosArray = resultado.data;
+
+            mostrarProductos(productosArray);
+        }
+    }
+    )
 });
