c6e032c015de408343c46a2b55cfa1fd47b0464d
francissilvera <88556900+francissilvera@users.noreply.github.com>
2021-09-12 18:36:38 -0300

    nuevo json de productos

--- js/products.js
+++ js/products.js
@@ -9,10 +9,10 @@ var productosArray = [];
 var minCost;
 var maxCost;
 
-// function verProducto (id) {
-    // localStorage.setItem("producto", JSON.stringify({productoId: id}));
-   // window.location = "product-info.html"
-// }
+function verProducto (id) {
+    localStorage.setItem("producto", JSON.stringify({productoId: id}));
+    window.location = "product-info.html"
+}
 
 function sortProductos (criterio, array) { 
     let result = [];
@@ -88,7 +88,7 @@ function mostrarProductos(array) {
                         <p class="mb-1">` + producto.cost + ` USD </p>
                         <small class="text-muted">` + producto.soldCount + ` artículos vendidos</small>
                         <br> <br>
-                        <button onclick=window.location="product-info.html">Ver producto</button>
+                        <button onclick="verProducto(` + producto.id + `)">Ver producto</button>
                         </div>
                     </div>
                 </div>`
