ca609011cdd001c19a4c7df96823b15fd4673f7d
francissilvera <88556900+francissilvera@users.noreply.github.com>
2021-09-12 19:09:16 -0300

    enlaces

--- js/product-info.js
+++ js/product-info.js
@@ -12,28 +12,25 @@ function mostrarProducto(producto){
    document.getElementById("productCategory").innerHTML = producto.category;
    document.getElementById("productCost").innerHTML = producto.cost + ` ` + producto.currency;
    document.getElementById("productSoldCount").innerHTML = producto.soldCount;
-   document.getElementById("productImages").innerHTML = `<img class="img" src="img/prod1_1.jpg" width="300" height="200" alt="">`
+   document.getElementById("productImages").innerHTML = producto.images;
 
 };
 
 document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCT_INFO_URL+id+".json").then(function (resultado) {
+    getJSONData(PRODUCT_INFO_URL+"/"+id+".json").then(function (resultado) {
         if (resultado.status === "ok") {
+            resultado.data.forEach(producto => {
+                if(producto.id == JSON.parse(localStorage.getItem("producto")).productoId){
             producto = resultado.data
         mostrarProducto(producto);
         }
+        });
+        };
     });
 });
-
-
-            // resultado.data.forEach(producto => {
-               // if(producto.id == JSON.parse(localStorage.getItem("producto").productoId)){
-                   // producto = resultado.data
-               // }
-           // })
             
  // ENLACES:
-  // https://francissilvera.github.io/e-mercado/js/productos-info_1.json
-  // https://francissilvera.github.io/e-mercado/js/productos-info_2.json
-  // https://francissilvera.github.io/e-mercado/js/productos-info_3.json
-  // https://francissilvera.github.io/e-mercado/js/productos-info_4.json
+  // https://francissilvera.github.io/e-mercado/js/productos-info/1.json
+  // https://francissilvera.github.io/e-mercado/js/productos-info/2.json
+  // https://francissilvera.github.io/e-mercado/js/productos-info/3.json
+  // https://francissilvera.github.io/e-mercado/js/productos-info/4.json
